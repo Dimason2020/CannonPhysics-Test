2a09209171ecd55fe071844caf6e74817820c20f
Dimason2020 <timmerklixx@gmail.com>
2023-10-15 11:03:37 +0300

    Update TrajectoryHandler.cs

--- Flexus/Assets/Game/Scripts/Trajectory/TrajectoryHandler.cs
+++ Flexus/Assets/Game/Scripts/Trajectory/TrajectoryHandler.cs
@@ -7,46 +7,55 @@ public class TrajectoryHandler : MonoBehaviour
     [SerializeField] private LineRenderer _line;
     [Inject] private InputHandler _inputHandler;
 
-    private void OnEnable()
-    {
-        _inputHandler.OnMoveInputChanged += GenerateGuide;
-        _inputHandler.OnSliderValueChanged += GenerateGuide;
-    }
-    private void OnDisable()
-    {
-        _inputHandler.OnMoveInputChanged -= GenerateGuide;
-        _inputHandler.OnSliderValueChanged -= GenerateGuide;
-    }
+    private Vector3 verticalDirection = Vector3.up;
+    private Vector3 horizontalDirection;
+
+    private float horizontalVelocity;
+    private float verticalVelocity;
+
+    private const float timeStep = 0.08f;
+    private const float gravity = 9.81f;
 
     private void Start()
     {
-        GenerateGuide();
+        UpdateTrajectory();
+
+        _inputHandler.OnMoveInputChanged += UpdateTrajectory;
+        _inputHandler.OnSliderValueChanged += UpdateTrajectory;
     }
 
-    private void GenerateGuide()
+    private void UpdateTrajectory()
     {
-        var direction           = transform.forward;
-        var verticalDirection   = Vector3.up;
-        var horizontalDirection = direction;
+        CalculateTrajectory();
+        RenderTrajectory();
+    }
+
+
+    private void CalculateTrajectory()
+    {
+        Vector3 forwardDirection = transform.forward;
+
+        horizontalDirection = forwardDirection;
         horizontalDirection.y = 0;
         horizontalDirection.Normalize();
-        
-        var velocity           = direction * _inputHandler.PowerValue;
-        var horizontalVelocity = new Vector2(velocity.x, velocity.z).magnitude;
-        var verticalVelocity   = velocity.y;
 
-        const float t = 0.08f;
-        
-        var point = transform.position;
+        Vector3 velocity = forwardDirection * _inputHandler.PowerValue;
+        horizontalVelocity = new Vector2(velocity.x, velocity.z).magnitude;
+        verticalVelocity = velocity.y;
+    }
+
+    private void RenderTrajectory()
+    {
+        Vector3 trajectoryPoint = transform.position;
         
         for (var i = 0; i < _line.positionCount; i++)
         {
-            _line.SetPosition(i, point);
+            _line.SetPosition(i, trajectoryPoint);
             
-            point += horizontalDirection * (horizontalVelocity * t);
-            point += verticalDirection   * (verticalVelocity   * t - 4.9f * t * t);
+            trajectoryPoint += horizontalDirection * (horizontalVelocity * timeStep);
+            trajectoryPoint += verticalDirection   * (verticalVelocity   * timeStep - 4.9f * timeStep * timeStep);
             
-            verticalVelocity -= 9.8f * t;
+            verticalVelocity -= gravity * timeStep;
         }
     }
 }
